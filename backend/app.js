5a2b7f2e9fafbeb84c943ff4a28ba665c922dae8
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2023-02-09 12:19:50 +0600

    Another route added

--- backend/app.js
+++ backend/app.js
@@ -9,6 +9,11 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
+app.get("/iut", (req, res) => {
+    res.status(200);
+    res.send("In IUT CGPA Matters");
+})
+
 app.listen(PORT, (err) => {
   if (!err) {
     console.log(`Server running on port ${PORT}`);
