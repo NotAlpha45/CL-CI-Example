1b184f90d80a023ca0b5bbfde2d52a95580e000a
Maheen Hoque <75400940+NotAlpha45@users.noreply.github.com>
2023-02-09 12:44:45 +0600

    major overhaul of repository

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+app = express();
+
+const PORT = process.env.PORT | 3000;
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Hello World");
+});
+
+app.get("/iut", (req, res) => {
+  res.status(200);
+  res.send("In IUT CGPA Matters");
+});
+
+app.listen(PORT, (err) => {
+  if (!err) {
+    console.log(`Server running on port ${PORT}`);
+  }
+});
